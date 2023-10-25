bf48f02724bc11a32e104b145195d338dffc01c3
sofymiranda <mirandaspfya@gmail.com>
2023-10-25 11:10:01 -0300

    atividade 21

---
+++ atv21.py
@@ -0,0 +1,9 @@
+# Exercício Python 21: Faça um programa que mostre na tela uma contagem regressiva para o estouro de
+# fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+import time
+print ("Os fogos vão explodir em: ")
+lista=[10,9,8,7,6,5,4,3,2,1,0]
+for (item) in (lista):
+ print (item)
+ time.sleep(1)
+else: print("pow")
